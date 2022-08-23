8004dc4f9cd62efe0e9b0f555225b3ffad59263d
Dinudash <dinesh1015485@gmail.com>
2022-08-23 14:57:49 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './products/product.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatDividerModule} from '@angular/material/divider'; 
+import { HttpClientModule } from '@angular/common/http';
+import { prodComponent } from './prods/prod.component';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+
+@NgModule({
+  declarations: [AppComponent, ProductComponent,prodComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDividerModule,
+   HttpClientModule,
+   ReactiveFormsModule,
+   MatFormFieldModule,
+   FormsModule ,
+   MatInputModule,
+   BrowserAnimationsModule,
+   MatPaginatorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
